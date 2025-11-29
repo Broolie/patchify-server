d6cb57fb0309db06f3aab873e2dc2a14f039e1ae
bratishchenkooi <bratishchenkooi@solit.by>
2025-11-30 02:07:51 +0300

    formatting

--- src/request_handler.rs
+++ src/request_handler.rs
@@ -1,14 +1,20 @@
-use std::{ops::BitOr, sync::Arc};
+use std::sync::Arc;
 
 use anyhow::{Result, anyhow};
 use flatbuffers::FlatBufferBuilder;
 use quinn::SendStream;
-use thiserror::Error;
 
 use crate::{
-    Session, common_generated::{AuthType, Common}, config::Config, constants::API_VERSION, errors::Errors, schemas::patchify::{
-        AuthRequest, AuthResponse, CommonRequest, CommonResponse, CommonResponseArgs, FetchAllRequest, FetchAllResponse, FetchRequest, FetchResponse, NeedAuthRequest, NeedAuthResponse, NeedAuthResponseArgs, PullRequest, PullResponse, Request, Response, ResponseUnionTableOffset, Status, StatusRequest, StatusResponse, VersionRequest, VersionResponse, finish_common_response_buffer
-    }
+    Session,
+    common_generated::{AuthType, Common},
+    config::Config,
+    constants::API_VERSION,
+    errors::Errors,
+    schemas::patchify::{
+        AuthRequest, CommonRequest, CommonResponse, CommonResponseArgs,
+        FetchAllRequest, FetchRequest, NeedAuthRequest,
+        NeedAuthResponse, NeedAuthResponseArgs, PullRequest, Request, Response, Status, StatusRequest, VersionRequest, finish_common_response_buffer,
+    },
 };
 
 pub async fn handle_request(
@@ -28,56 +34,77 @@ pub async fn handle_request(
     let request = flatbuffers::root::<CommonRequest>(&request)?;
 
     let response = match request.request_type() {
-        Request::AuthRequest => handle_auth(
-            config,
-            session,
-            request
-                .request_as_auth_request()
-                .expect("AuthRequest unwrap"),
-        ).await?,
-        Request::FetchRequest => handle_fetch(
-            config,
-            session,
-            request
-                .request_as_fetch_request()
-                .expect("FetchRequest unwrap"),
-        ).await?,
-        Request::FetchAllRequest => handle_fetch_all(
-            config,
-            session,
-            request
-                .request_as_fetch_all_request()
-                .expect("FetchAllRequest unwrap"),
-        ).await?,
-        Request::NeedAuthRequest => handle_need_auth(
-            config,
-            session,
-            request
-                .request_as_need_auth_request()
-                .expect("NeedAuthRequest unwrap"),
-        ).await?,
-        Request::PullRequest => handle_pull(
-            config,
-            session,
-            request
-                .request_as_pull_request()
-                .expect("PullRequest unwrap"),
-            &mut send,
-        ).await?,
-        Request::StatusRequest => handle_status(
-            config,
-            session,
-            request
-                .request_as_status_request()
-                .expect("StatusRequest unwrap"),
-        ).await?,
-        Request::VersionRequest => handle_version(
-            config,
-            session,
-            request
-                .request_as_version_request()
-                .expect("VersionRequest unwrap"),
-        ).await?,
+        Request::AuthRequest => {
+            handle_auth(
+                config,
+                session,
+                request
+                    .request_as_auth_request()
+                    .expect("AuthRequest unwrap"),
+            )
+            .await?
+        }
+        Request::FetchRequest => {
+            handle_fetch(
+                config,
+                session,
+                request
+                    .request_as_fetch_request()
+                    .expect("FetchRequest unwrap"),
+            )
+            .await?
+        }
+        Request::FetchAllRequest => {
+            handle_fetch_all(
+                config,
+                session,
+                request
+                    .request_as_fetch_all_request()
+                    .expect("FetchAllRequest unwrap"),
+            )
+            .await?
+        }
+        Request::NeedAuthRequest => {
+            handle_need_auth(
+                config,
+                session,
+                request
+                    .request_as_need_auth_request()
+                    .expect("NeedAuthRequest unwrap"),
+            )
+            .await?
+        }
+        Request::PullRequest => {
+            handle_pull(
+                config,
+                session,
+                request
+                    .request_as_pull_request()
+                    .expect("PullRequest unwrap"),
+                &mut send,
+            )
+            .await?
+        }
+        Request::StatusRequest => {
+            handle_status(
+                config,
+                session,
+                request
+                    .request_as_status_request()
+                    .expect("StatusRequest unwrap"),
+            )
+            .await?
+        }
+        Request::VersionRequest => {
+            handle_version(
+                config,
+                session,
+                request
+                    .request_as_version_request()
+                    .expect("VersionRequest unwrap"),
+            )
+            .await?
+        }
         _ => return Err(Errors::InvalidRequest.into()),
     };
 
@@ -90,19 +117,35 @@ pub async fn handle_request(
     Ok(())
 }
 
-async fn handle_auth<'a>(config: Arc<Config>, session: Arc<Session>, request: AuthRequest<'a>) -> Result<Vec<u8>> {
+async fn handle_auth<'a>(
+    config: Arc<Config>,
+    session: Arc<Session>,
+    request: AuthRequest<'a>,
+) -> Result<Vec<u8>> {
     todo!()
 }
 
-async fn handle_fetch<'a>(config: Arc<Config>, session: Arc<Session>, request: FetchRequest<'a>) -> Result<Vec<u8>> {
+async fn handle_fetch<'a>(
+    config: Arc<Config>,
+    session: Arc<Session>,
+    request: FetchRequest<'a>,
+) -> Result<Vec<u8>> {
     todo!()
 }
 
-async fn handle_fetch_all<'a>(config: Arc<Config>, session: Arc<Session>, request: FetchAllRequest<'a>) -> Result<Vec<u8>> {
+async fn handle_fetch_all<'a>(
+    config: Arc<Config>,
+    session: Arc<Session>,
+    request: FetchAllRequest<'a>,
+) -> Result<Vec<u8>> {
     todo!()
 }
 
-async fn handle_need_auth<'a>(config: Arc<Config>, session: Arc<Session>, request: NeedAuthRequest<'a>) -> Result<Vec<u8>> {
+async fn handle_need_auth<'a>(
+    config: Arc<Config>,
+    session: Arc<Session>,
+    request: NeedAuthRequest<'a>,
+) -> Result<Vec<u8>> {
     let mut builder = FlatBufferBuilder::new();
     let mut condense = AuthType::empty();
     for flag in config.available_auth_types.iter() {
@@ -110,37 +153,46 @@ async fn handle_need_auth<'a>(config: Arc<Config>, session: Arc<Session>, reques
     }
 
     let need_auth_args = NeedAuthResponseArgs {
-        auth_type: condense
+        auth_type: condense,
     };
 
     let need_auth_wip = NeedAuthResponse::create(&mut builder, &need_auth_args);
-    
+
     let args = CommonResponseArgs {
         common: Some(&Common::new(API_VERSION)),
         status: Status::Ok,
         response_type: Response::NeedAuthResponse,
-        response: Some(need_auth_wip.as_union_value())
+        response: Some(need_auth_wip.as_union_value()),
     };
-    
+
     let response_wip = CommonResponse::create(&mut builder, &args);
     finish_common_response_buffer(&mut builder, response_wip);
-    let response = builder.finished_data(); 
-    
+    let response = builder.finished_data();
+
     Ok(response.to_vec())
 }
 
 async fn handle_pull<'a>(
-    config: Arc<Config>, session: Arc<Session>,
+    config: Arc<Config>,
+    session: Arc<Session>,
     request: PullRequest<'a>,
     send: &mut SendStream,
 ) -> Result<Vec<u8>> {
     todo!()
 }
 
-async fn handle_status<'a>(config: Arc<Config>, session: Arc<Session>, request: StatusRequest<'a>) -> Result<Vec<u8>> {
+async fn handle_status<'a>(
+    config: Arc<Config>,
+    session: Arc<Session>,
+    request: StatusRequest<'a>,
+) -> Result<Vec<u8>> {
     todo!()
 }
 
-async fn handle_version<'a>(config: Arc<Config>, session: Arc<Session>, request: VersionRequest<'a>) -> Result<Vec<u8>> {
+async fn handle_version<'a>(
+    config: Arc<Config>,
+    session: Arc<Session>,
+    request: VersionRequest<'a>,
+) -> Result<Vec<u8>> {
     todo!()
 }
